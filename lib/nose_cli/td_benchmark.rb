51b5e725696c3029b8071197227a4eccb3adc3fe
Y-Wakuta <prsprivatestock@i.softbank.jp>
2019-10-28 15:35:08 -0400

    check benchmark work for queries

--- lib/nose_cli/td_benchmark.rb
+++ lib/nose_cli/td_benchmark.rb
@@ -16,7 +16,24 @@ module NoSE
         prepare the target database.
       LONGDESC
 
+      # by benchmark command
       shared_option :mix
+      option :num_iterations, type: :numeric, default: 100,
+             banner: 'ITERATIONS',
+             desc: 'the number of times to execute each ' \
+                                    'statement'
+      option :repeat, type: :numeric, default: 1,
+             desc: 'how many times to repeat the benchmark'
+      option :group, type: :string, default: nil, aliases: '-g',
+             desc: 'restrict the benchmark to statements in the ' \
+                           'given group'
+      option :fail_on_empty, type: :boolean, default: true,
+             desc: 'abort if a column family is empty'
+      option :totals, type: :boolean, default: false, aliases: '-t',
+             desc: 'whether to include group totals in the output'
+      option :format, type: :string, default: 'txt',
+             enum: %w(txt csv), aliases: '-f',
+             desc: 'the format of the output data'
 
       # by create command
       option :dry_run, type: :boolean, default: false,
@@ -47,12 +64,94 @@ module NoSE
         (0...result.timesteps).each do |timestep|
           puts "\e[33m timestep: #{timestep} ================================== \e[0m"
 
-          indexes = result.time_depend_indexes.indexes_all_timestep[timestep].indexes
-          #index_values = index_values indexes, backend,
-          #                            options[:num_iterations],
-          #                            options[:fail_on_empty]
-          #group_tables = Hash.new { |h, k| h[k] = [] }
-          #group_totals = Hash.new { |h, k| h[k] = 0 }
+          indexes_for_this_timestep = result.time_depend_indexes.indexes_all_timestep[timestep].indexes
+          index_values = index_values indexes_for_this_timestep, backend,
+                                      options[:num_iterations],
+                                      options[:fail_on_empty]
+          group_tables = Hash.new { |h, k| h[k] = [] }
+          group_totals = Hash.new { |h, k| h[k] = 0 }
+
+          result.time_depend_plans.map{|tdp| tdp.plans[timestep]}.each do |plan|
+            query = plan.query
+            weight = result.workload.statement_weights[query]
+            next if query.is_a?(SupportQuery) || !weight
+            @logger.debug { "Executing #{query.text}" }
+            puts "Executing Query: #{query.text}"
+            puts "    Executing Plan: #{plan.inspect}"
+
+            next unless options[:group].nil? || plan.group == options[:group]
+
+            indexes = plan.select do |step|
+              step.is_a? Plans::IndexLookupPlanStep
+            end.map(&:index)
+
+            measurement = bench_query backend, indexes, plan, index_values,
+                                      options[:num_iterations], options[:repeat],
+                                      weight: weight
+            next if measurement.empty?
+
+            measurement.estimate = plan.cost
+            group_totals[plan.group] += measurement.mean
+            group_tables[plan.group] << measurement
+          end
+
+          result.workload.updates.each do |update|
+            weight = result.workload.statement_weights[update]
+            next unless weight
+
+            plans = (result.update_plans || []).select do |possible_plan|
+              possible_plan.statement == update
+            end
+            next if plans.empty?
+
+            @logger.debug { "Executing #{update.text}" }
+            puts "Executing #{update.text}"
+
+            plans.each do |plan|
+              next unless options[:group].nil? || plan.group == options[:group]
+
+              # Get all indexes used by support queries
+              indexes = plan.query_plans.flat_map(&:indexes) << plan.index
+
+              measurement = bench_update backend, indexes, plan, index_values,
+                                         options[:num_iterations],
+                                         options[:repeat], weight: weight
+              next if measurement.empty?
+
+              measurement.estimate = plan.cost
+              group_totals[plan.group] += measurement.mean
+              group_tables[plan.group] << measurement
+            end
+          end
+
+          total = 0
+          table = []
+          group_totals.each do |group, group_total|
+            total += group_total
+            total_measurement = Measurements::Measurement.new nil, 'TOTAL'
+            group_table = group_tables[group]
+            total_measurement << group_table.map{|gt| gt.weighted_mean(timestep)} \
+                               .inject(0, &:+)
+            group_table << total_measurement if options[:totals]
+            table << OpenStruct.new(label: label, group: group,
+                                    measurements: group_table)
+          end
+
+          if options[:totals]
+            total_measurement = Measurements::Measurement.new nil, 'TOTAL'
+            total_measurement << table.map do |group|
+              group.measurements.find { |m| m.name == 'TOTAL' }.mean
+            end.inject(0, &:+)
+            table << OpenStruct.new(label: label, group: 'TOTAL',
+                                    measurements: [total_measurement])
+          end
+
+          case options[:format]
+          when 'txt'
+            output_table table
+          else
+            output_csv table
+          end
 
           break if timestep == result.timesteps - 1
           exec_migration(result, backend, timestep)
@@ -67,8 +166,8 @@ module NoSE
       def set_up_db(result, backend, loader)
         indexes = result.time_depend_indexes.indexes_all_timestep.first.indexes
         # Produce the DDL and execute unless the dry run option was given
-        backend.create_time_depend_indexes(indexes, !options[:dry_run], options[:skip_existing],
-                                           options[:drop_existing])
+        backend.create_indexes(indexes, !options[:dry_run], options[:skip_existing],
+                                           options[:drop_existing]).each {|ddl| puts ddl}
 
         # Create a new instance of the loader class and execute
         loader.load indexes, options[:loader], options[:progress],
@@ -107,9 +206,7 @@ module NoSE
 
         migrate_plan.new_plan.steps.map{|step| step.index}.each do |new_index|
           unless backend.index_exists?(new_index)
-            Enumerator.new do |enum|
-              backend.create_index(new_index, enum, !options[:dry_run], options[:skip_existing])
-            end.each {|ddl| puts ddl}
+            puts backend.create_index(new_index, !options[:dry_run], options[:skip_existing])
             backend.index_insert(new_index, obsolete_rows)
           end
         end
