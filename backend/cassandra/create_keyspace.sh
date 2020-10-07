4f5d1dabedb85257fb211da9413cb452b3d4a4a0
Y-Wakuta <prsprivatestock@i.softbank.jp>
2020-10-07 19:10:21 +0900

    added TPC-H on MySQL

--- backend/cassandra/create_keyspace.sh
+++ backend/cassandra/create_keyspace.sh
@@ -1,1 +1,1 @@
-cqlsh -e "CREATE KEYSPACE rubis WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':1};"
+cqlsh -e "CREATE KEYSPACE tpch WITH REPLICATION = {'class':'SimpleStrategy','replication_factor':1};"
--- backend/up_backend.sh
+++ backend/up_backend.sh
@@ -6,8 +6,9 @@ docker-compose down
 docker-compose build --no-cache
 docker-compose up -d
 
-sleep 20
-
+sleep 70
 docker exec cassandra_migrate bash create_keyspace.sh
+docker exec mysql_migrate bash setup_db.sh
+
 #docker cp ../../rubis_basic.sql mysql_migrate:/
 #docker exec mysql_migrate mysql -uroot -proot -Drubis < /rubis_basic.sql
