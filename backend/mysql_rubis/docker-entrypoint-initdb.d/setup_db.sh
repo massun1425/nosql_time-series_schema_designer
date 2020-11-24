d64846d11ab9261906f801741fc84f76e6918fd1
Y-Wakuta <prsprivatestock@i.softbank.jp>
2020-11-24 16:36:08 +0900

    added split rubis backend and tpch backend

---
+++ backend/mysql_rubis/docker-entrypoint-initdb.d/setup_db.sh
@@ -0,0 +1,2 @@
+mysql -uroot -proot  -Drubis < /experiments/rubis/rubis-schema.sql
+mysql -uroot -proot  -Drubis < /experiments/rubis/rubis-update.sql
--- backend/up_backend.sh
+++ backend/up_backend.sh
@@ -8,7 +8,7 @@ docker-compose up -d
 
 sleep 70
 docker exec cassandra_migrate bash create_keyspace.sh
-docker exec mysql_migrate bash setup_db.sh
+docker exec mysql_tpch bash setup_db.sh
 
 #docker cp ../../rubis_basic.sql mysql_migrate:/
 #docker exec mysql_migrate mysql -uroot -proot -Drubis < /rubis_basic.sql
