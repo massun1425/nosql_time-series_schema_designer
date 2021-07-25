8020b606dc85dddac804613cd601eae75e5072bb
Y-Wakuta <yusuke.wakuta@gmail.com>
2021-07-26 00:48:45 +0900

    save calibrate_query_cost command for early July cost calibration

--- lib/nose_cli/calibrate_query_cost.rb
+++ lib/nose_cli/calibrate_query_cost.rb
@@ -18,22 +18,33 @@ module NoSE
         options[:index_cost] = options[:partition_cost] = options[:row_cost] = 0.1
         workload = Workload.new{|_| Model('tpch_card_key_composite_dup_lineitems_order_customer')}
 
-        record_points = {:rows_coeff => [], :parts_coeff => [], :latency => []}
         query1 = Statement.parse 'SELECT l_orderkey.*, lineitem.* FROM lineitem.l_orderkey.o_custkey '\
                                       'WHERE o_custkey.c_custkey = ?', workload.model
-        record_points = calibrate_query_coeff query1, workload, record_points
+        calibrate_for_query query1, workload,[0, 1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 100, 200, 300], [1e-2, 5 * 1e-2, 0.1, 0.4, 0.7, 1.0]
+        #calibrate_for_query query1, workload,[100, 200], [0.1, 0.5, 1.0]
 
-        query2 = Statement.parse 'SELECT l_orderkey.*, lineitem.* FROM lineitem.l_orderkey.o_custkey '\
-                                      'WHERE o_custkey.c_mktsegment = ?', workload.model
-        record_points = calibrate_query_coeff query2, workload, record_points
 
-        hash_array_to_csv record_points
-        multi_regression record_points, [:parts_coeff, :rows_coeff], :latency
+        #query1 = Statement.parse 'SELECT l_orderkey.*, lineitem.* FROM lineitem.l_orderkey.o_custkey '\
+        #                              'WHERE lineitem.l_orderkey = ?', workload.model
+        ##calibrate_for_query query1, workload,[0, 1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 100, 200, 300], [1e-2, 5 * 1e-2, 0.1, 0.4, 0.7, 1.0]
+        #calibrate_for_query query1, workload,[100, 200], [0.1, 0.5, 1.0]
+
+
+        #query1 = Statement.parse 'SELECT l_orderkey.*, lineitem.* FROM lineitem.l_orderkey.o_custkey '\
+        #                              'WHERE o_custkey.c_custkey = ?', workload.model
+        #calibrate_for_query query1, workload,[0, 1, 2, 3, 4, 5, 10, 20, 30, 40, 50, 100, 200, 300], [1e-2, 5 * 1e-2, 0.1, 0.4, 0.7, 1.0]
+        #calibrate_for_query query1, workload,[100, 200], [0.1, 0.5, 1.0]
       end
 
       private
 
-      def calibrate_query_coeff(query, workload, record_points)
+      def calibrate_for_query(query, workload, get_nums, reduction_ratios)
+        record_points = calibrate_query_coeff query, workload, get_nums, reduction_ratios
+        hash_array_to_csv record_points
+        multi_regression record_points, [:parts_coeff, :rows_coeff], :latency
+      end
+
+      def calibrate_query_coeff(query, workload, get_nums, reduction_rates)
 
         # get the COUNT of target CF
         # base_cost + a (# of get) + b (# of returned records)
@@ -41,6 +52,7 @@ module NoSE
         # ここの割合が (# of returned records) を　x% 倍した値として，レコードセットを取得する．
         # (# of get) は意図的に変化させる．(# of get) は変化させると，その変化分を (# of returned records) にも反映する必要があるので，注意
 
+        record_points = {:rows_coeff => [], :parts_coeff => [], :latency => [], :raw_rows_coeff => [], :raw_field_size => []}
         index = query.materialize_view
         backend = Backend::CassandraBackend.new(workload.model, [index], nil, nil, options[:backend])
         loader = get_class('loader', options).new workload, backend
@@ -61,30 +73,29 @@ module NoSE
         full_record_size = full_values.size
         STDERR.puts "whole loading time: " + loading_time.to_s
 
-        loop_times = 3
-        get_nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40]
-        reduction_rates = [1e-6, 5 * 1e-6, 1e-5, 5 * 1e-5, 1e-4, 5 * 1e-4, 1e-3, 5* 1e-3, 0.01, 0.04, 0.07, 0.1].map{|i| i * 0.01}
+        loop_times = 2
         loop_times.times do |_|
-          get_nums.each do |get_num|
-            reduction_rates.each do |reduction_rate|
-              # ここで，100% のレコード数と本来の index.entries を比較して，レコード数が少ない場合でも対応してあげる必要がある．ただし，migration_cost の場合と違って，全体のレコード数に対してどのくらい応答レコード数が比例するかは微妙
-              target_record_size = (full_record_size * reduction_rate).to_i
-              real_reduction_rate = (target_record_size / index.entries.to_f).round(10)
-              puts "start benchmark for #{target_record_size}"
-              backend.recreate_index(index, !options[:dry_run], options[:skip_existing], true)
+          reduction_rates.each do |reduction_rate|
+            target_record_size = (full_record_size * reduction_rate).to_i
+            puts "start benchmark for #{target_record_size}"
+            backend.recreate_index(index, !options[:dry_run], options[:skip_existing], true)
 
-              sampled_values = full_values.sample(target_record_size, random: Object::Random.new(100))
-              backend.load_index_by_cassandra_loader index, sampled_values
+            sampled_values = full_values.sample(target_record_size, random: Object::Random.new(100))
+            backend.load_index_by_cassandra_loader index, sampled_values
 
+            get_nums.each do |get_num|
               conditions = get_conditions backend, index, plan, sampled_values, get_num
 
               elapse = measure_latency backend, plan, conditions
 
               record_points[:parts_coeff] << [parts_coeff * get_num, conditions.size].min
 
+              real_reduction_rate = (target_record_size / index.entries.to_f).round(10)
               # the number of returned row would not smaller than 1
               record_points[:rows_coeff] << [rows_coeff * real_reduction_rate * get_num * step.fields.sum_by(&:size),
-                                             conditions.size > 0 ? 1 : 0].max
+                                             conditions.size > 0 ? step.fields.sum_by(&:size) : 0].max
+              record_points[:raw_rows_coeff] << rows_coeff
+              record_points[:raw_field_size] << step.fields.sum_by(&:size)
               record_points[:latency] << elapse
             end
           end
@@ -100,7 +111,7 @@ module NoSE
         puts lr.summary
         puts lr.constant
         x_vars.each do |x_var|
-          puts lr.coeffs[x_var]
+          puts "#{x_var}: #{lr.coeffs[x_var]}"
         end
         puts "================================================="
         return Proc.new do |variable_kv_hash|
