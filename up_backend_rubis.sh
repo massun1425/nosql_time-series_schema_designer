1f0e5e27465eb71871d66cc5aee804278ff877f6
Y-Wakuta <yusuke.wakuta@gmail.com>
2021-08-13 01:39:46 +0900

    save current docker-compose file for RUBIS to delete

--- up_backend_rubis.sh
+++ up_backend_rubis.sh
@@ -6,7 +6,9 @@ docker-compose -f docker-compose_rubis.yml down
 COMPOSE_DOCKER_CLI_BUILD=1 DOCKER_BUILDKIT=1 docker-compose -f docker-compose_rubis.yml build --no-cache --parallel
 docker-compose -f docker-compose_rubis.yml up -d
 
-sleep 50
-docker exec cassandra_migrate bash create_keyspace_rubis.sh
-docker exec -ti faker_client node one_tenth_fake.js
+sleep 100
+docker cp ./backend/cassandra/create_keyspace_rubis.sh c1_wakuta:/
+docker exec c1_wakuta bash create_keyspace_rubis.sh
+docker exec -ti faker_client node fake.js
+docker exec -ti mysql_rubis bash -c "mysql -uroot -proot -Drubis < /additional_index.sql"
 
